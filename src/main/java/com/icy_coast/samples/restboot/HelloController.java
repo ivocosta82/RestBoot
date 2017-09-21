473c1d20cd3a51e6c217c32f50d11fada980ccbb
Ivo Costa <spacemanpt@gmail.com>
2017-09-21 13:42:11 +0100

    Initial commit

---
+++ src/main/java/com/icy_coast/samples/restboot/HelloController.java
@@ -0,0 +1,13 @@
+package com.icy_coast.samples.restboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
